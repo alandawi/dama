7d0920b1963c6c3038e18e07f7238396e6bb76db
emilianooferreyra <emilianooferreyra@gmail.com>
2022-01-06 16:10:03 -0300

    Add all files in a single zip file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,113 +1,129 @@
-'use strict';
+"use strict";
 
-import yaml from 'js-yaml';
-import browser from 'browser-sync';
-import rimraf from 'rimraf';
-import fs from 'fs';
-import gulp from 'gulp';
-import mjmlGulp from 'gulp-mjml';
-import mjml from 'mjml';
-import nunjucks from 'gulp-nunjucks-render';
-import data from 'gulp-data';
-import imagemin from 'gulp-imagemin';
+import yaml from "js-yaml";
+import browser from "browser-sync";
+import rimraf from "rimraf";
+import fs from "fs";
+import gulp from "gulp";
+import mjmlGulp from "gulp-mjml";
+import mjml from "mjml";
+import nunjucks from "gulp-nunjucks-render";
+import data from "gulp-data";
+import imagemin from "gulp-imagemin";
+import zip from "gulp-zip";
 
 const PATHS = {
-  src: './src/{layouts,partials,templates}/**/*',
+  src: "./src/{layouts,partials,templates}/**/*",
   mjml: {
-    src: './build/mjml/**/*.mjml',
-    build: './build/mjml/',
+    src: "./build/mjml/**/*.mjml",
+    build: "./build/mjml/",
   },
-  build: './build/html/',
-  data: './src/data/data.yml',
-  layouts: './src/layouts/',
-  partials: './src/partials/',
-  preview: './src/preview/',
-  images: './src/templates/**/images/*',
-  templates: './src/templates/**/*.mjml'
-}
+  build: "./build/html/",
+  data: "./src/data/data.yml",
+  layouts: "./src/layouts/",
+  partials: "./src/partials/",
+  preview: "./src/preview/",
+  images: "./src/templates/**/images/*",
+  templates: "./src/templates/**/*.mjml",
+};
 
 let templatesList = [];
 
 function loadData() {
-  let file = fs.readFileSync(PATHS.data, 'utf8')
+  let file = fs.readFileSync(PATHS.data, "utf8");
   return yaml.load(file);
 }
 
 function getTemplates(done) {
-  return fs.readdir('./src/templates', { withFileTypes: true }, (err, files) => {
+  return fs.readdir(
+    "./src/templates",
+    { withFileTypes: true },
+    (err, files) => {
       if (err) {
-          return console.log('Unable to scan directory: ' + err);
+        return console.log("Unable to scan directory: " + err);
       }
 
       templatesList.length = 0;
 
       files.forEach((file) => {
-          if (file.isDirectory()) {
-            templatesList.push({
-              title: file.name.split('_').join(' '),
-              dirName: file.name
-            })
-          }
+        if (file.isDirectory()) {
+          templatesList.push({
+            title: file.name.split("_").join(" "),
+            dirName: file.name,
+          });
+        }
       });
 
       done();
-  });
+    }
+  );
 }
 
 function clean(done) {
-  rimraf('./build/*', done);
+  rimraf("./build/*", done);
 }
 
 function buildPreview() {
-  return gulp.src(`${PATHS.preview}index.html`)
-    .pipe(nunjucks({
-      data: {
-        columns: 3, // TODO need to set this value into the preview
-        templates: templatesList
-      },
-      envOptions: {
-        noCache: true
-      },
-      inheritExtension: true
-    }))
+  return gulp
+    .src(`${PATHS.preview}index.html`)
+    .pipe(
+      nunjucks({
+        data: {
+          columns: 3, // TODO need to set this value into the preview
+          templates: templatesList,
+        },
+        envOptions: {
+          noCache: true,
+        },
+        inheritExtension: true,
+      })
+    )
     .pipe(gulp.dest(PATHS.build));
 }
 
 function buildTemplates() {
-  return gulp.src(PATHS.templates)
+  return gulp
+    .src(PATHS.templates)
     .pipe(data(loadData))
-    .pipe(nunjucks({
-      path: [
-        PATHS.layouts,
-        PATHS.partials
-      ],
-      envOptions: {
-        noCache: true
-      },
-      inheritExtension: true
-    }))
+    .pipe(
+      nunjucks({
+        path: [PATHS.layouts, PATHS.partials],
+        envOptions: {
+          noCache: true,
+        },
+        inheritExtension: true,
+      })
+    )
     .pipe(gulp.dest(PATHS.mjml.build));
 }
 
 function buildMJML() {
-  return gulp.src(PATHS.mjml.src)
+  return gulp
+    .src(PATHS.mjml.src)
     .pipe(mjmlGulp(mjml, { beautify: true, minify: false }))
     .pipe(gulp.dest(PATHS.build));
 }
 
 function buildImages() {
-  return gulp.src(['./src/templates/**', '!./src/templates/**/*.mjml'])
-    .pipe(imagemin([
-      imagemin.gifsicle({interlaced: true}),
-      imagemin.mozjpeg({quality: 80, progressive: true}),
-      imagemin.optipng({optimizationLevel: 5}),
-      imagemin.svgo({
-        plugins: [
-          {removeViewBox: false},
-          {cleanupIDs: false}
-        ]
-      })
-    ]))
+  return gulp
+    .src(["./src/templates/**", "!./src/templates/**/*.mjml"])
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 80, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [{ removeViewBox: false }, { cleanupIDs: false }],
+        }),
+      ])
+    )
+    .pipe(gulp.dest(PATHS.build));
+}
+
+function buildZip() {
+  return gulp
+    .src(["build/html/**", "!./build/html/index.html"])
+    .pipe(zip("output.zip"))
     .pipe(gulp.dest(PATHS.build));
 }
 
@@ -115,21 +131,34 @@ function setServer(done) {
   browser.init({
     server: {
       baseDir: PATHS.build,
-      directory: false
+      directory: false,
     },
-    port: '9000',
-    notify: false
+    port: "9000",
+    notify: false,
   });
 
   done();
 }
 
 function watchFiles() {
-  gulp.watch([PATHS.src, PATHS.data], gulp.series('build')); // browser.reload
+  gulp.watch([PATHS.src, PATHS.data], gulp.series("build")); // browser.reload
 }
 
-gulp.task('build',
-  gulp.series(clean, getTemplates, buildPreview, buildTemplates, buildTemplates, buildMJML, buildImages));
+gulp.task(
+  "build",
+  gulp.series(
+    clean,
+    getTemplates,
+    buildPreview,
+    buildTemplates,
+    buildTemplates,
+    buildMJML,
+    buildImages,
+    buildZip
+  )
+);
 
-gulp.task('default',
-  gulp.series('build', setServer, gulp.parallel(watchFiles)));
+gulp.task(
+  "default",
+  gulp.series("build", setServer, gulp.parallel(watchFiles))
+);
